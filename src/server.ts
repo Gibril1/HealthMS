ca4ba0e222dfe6cfb861a86d1364d1f32fe7b97a
Gibril1 <gibrilissakaiddrisu@gmail.com>
2023-01-18 11:31:54 -0800

    server setup

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+const express = require('express')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+
+
+// Init app
+const app = express()
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+// app.use(errorHandler)
+
+// Listen to app
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
